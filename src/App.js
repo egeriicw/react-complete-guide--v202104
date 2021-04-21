8965de36f90646aae2e506ee6b8b00efefddc9ef
Bill Eger <egeriicw@gmail.com>
2021-04-21 01:11:54 -0400

    Renamed to ExpensesFilter + added to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import ExpensesFilter from './components/Expenses/ExpensesFilter';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
